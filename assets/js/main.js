ab0b1259a94d22768c99f56deacc9fa8487ce9b7
gsquevedo <gsquevedo@inf.ufsm.br>
2023-04-03 22:02:25 -0300

    ajustes js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
 
-    let navbarlinks = $('#navbar .scrollto');
-    const navbarlinksActive = () => {
-      let position = $(window).scrollTop() + 200;
-      navbarlinks.each(function() {
-        if (!$(this).attr('href')) return;
-        let section = $($(this).attr('href'));
-        if (!section.length) return;
-        if (position >= section.offset().top && position <= (section.offset().top + section.outerHeight())) {
-          $(this).addClass('active');
-        } else {
-          $(this).removeClass('active');
-        }
-      });
-    }
-    navbarlinksActive();
-    $(window).scroll(navbarlinksActive);
-  
-
     const scrollto = (el) => {
       let elementPos = $(el).offset().top;
       $('html, body').animate({
@@ -39,23 +21,6 @@ $(document).ready(function() {
       $(window).scroll(toggleBacktotop);
     }
   
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $(this).toggleClass('bi-list bi-x');
-    });
-  
-    $(document).on('click', '.scrollto', function(e) {
-      if ($(this).attr('href')) {
-        e.preventDefault();
-        let body = $('body');
-        if (body.hasClass('mobile-nav-active')) {
-          body.removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle').toggleClass('bi-list bi-x');
-        }
-        scrollto($(this).attr('href'));
-      }
-    });
-
     if (window.location.hash) {
       let section = $(window.location.hash);
       if (section.length) {
